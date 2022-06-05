950199d286cf2378521a0f434939c7797be8d985
Joona Piirainen <joona.piirainen@gmail.com>
2022-06-05 14:33:51 +0300

    Feat: Add closures

--- lic.clj
+++ lic.clj
@@ -2,8 +2,10 @@
 (refer-clojure :exclude [eval read read-string])
 (require '[clojure.edn :refer [read read-string]])
 
+(def closure? (partial seq-starts-with? 'clo))
+
 (def literal?
-  (some-fn string? number? boolean? char? nil? fn?))
+  (some-fn string? number? boolean? char? nil? fn? closure?))
 
 (defn seq-starts-with? [starts-with form]
   (and (seqable? form) (= (first form) starts-with)))
@@ -15,20 +17,31 @@
 (def if? (partial seq-starts-with? 'if))
 
 (def env (atom {:globe {'+     + '= = 'list list 'map map 'concat concat
-                        'first first 'second second 'not not 'fav-num 41}}))
+                        'first first 'second second 'not not 'fav-num 41}
+                :scope {}}))
 
 (defn lookup-symbol [env-atom sym]
-  (let [{:keys [globe]} @env-atom
-        v (get globe sym)]
+  (let [{:keys [globe scope]} @env-atom
+        v (get (merge globe scope) sym)]
     (assert v (format "expected value for sym = %s" sym))
     v))
 
 (defn eval-many [env forms] (map (partial eval env) forms))
 
+(defn assign-vars [symbols args]
+  (assert (= (count symbols) (count args))
+          (format "symbols and args must match. symbols: %s args: %s"
+                  (vec symbols) (vec args)))
+  (into {} (map vector symbols args)))
+
+(defn eval-closure [env [_ symbols body] args]
+  (user/eval (atom (swap! env assoc :scope (assign-vars symbols args))) body))
+
 (defn eval-application [env [f & args]]
   (let [f-evaluated (user/eval env f)]
     (cond
-      (fn? f-evaluated) (apply f-evaluated (eval-many env args)))))
+      (fn? f-evaluated) (apply f-evaluated (eval-many env args))
+      (closure? f-evaluated) (eval-closure env f-evaluated (eval-many env args)))))
 
 (defn eval-def [env [_ k v]]
   (assert (symbol? k) (format "expected k = %s to be a symbol" k))
@@ -56,4 +69,5 @@
   (eval env (read-string "(def second-fav (+ 1 fav-num))"))
   (eval env (read-string "second-fav"))
   (eval env (read-string "(if (= fav-num 41) (quote yes) (quote no))"))
+  (eval env (read-string "((clo (x) (+ x fav-num)) 2)"))
   )
