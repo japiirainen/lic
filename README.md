96f651a9e3023c27b0665db0cd10cb168b0ab75d
Joona Piirainen <joona.piirainen@gmail.com>
2022-06-05 16:43:53 +0300

    Feat: REPL

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Lisp In Clojure (lic).
+
+### run lic REPL
+
+This assumes you have babashka installed.
+
+```sh
+bb -m lic
+```
--- lic.clj
+++ lic.clj
@@ -1,32 +1,24 @@
-#!/usr/bin/env bb
-(refer-clojure :exclude [eval read read-string])
-(require '[clojure.edn :refer [read read-string]])
-
-(def closure? (partial seq-starts-with? 'clo))
-
-(def literal?
-  (some-fn string? number? boolean? char? nil? fn? closure?))
+(ns lic
+  (:refer-clojure :exclude [read])
+  (:require [clojure.edn :refer [read]]))
 
 (defn seq-starts-with? [starts-with form]
   (and (seqable? form) (= (first form) starts-with)))
-
+(def closure? (partial seq-starts-with? 'clo))
+(def macro? (partial seq-starts-with? 'mac))
+(def literal?
+  (some-fn string? number? boolean? char? nil? fn? closure? macro?))
 (def quote? (partial seq-starts-with? 'quote))
-
 (def def? (partial seq-starts-with? 'def))
-
 (def if? (partial seq-starts-with? 'if))
-
-(def env (atom {:globe {'+     + '= = 'list list 'map map 'concat concat
-                        'first first 'second second 'not not 'fav-num 41}
-                :scope {}}))
 
 (defn lookup-symbol [env-atom sym]
   (let [{:keys [globe scope]} @env-atom
-        v (get (merge globe scope {'scope scope}) sym)]
-    (assert v (format "expected value for sym = %s" sym))
+        v (get (merge {'scope scope} globe scope) sym)]
+    (assert v (format "expected value for symbol = %s" sym))
     v))
 
-(defn eval-many [env forms] (map (partial eval env) forms))
+(declare evaluate eval-many)
 
 (defn assign-vars [symbols args]
   (assert (= (count symbols) (count args))
@@ -35,23 +27,29 @@
   (into {} (map vector symbols args)))
 
 (defn eval-closure [env [_ scope symbols body] args]
-  (user/eval (atom (swap! env assoc :scope (merge scope (assign-vars symbols args)))) body))
+  (evaluate (atom (assoc @env :scope (merge scope (assign-vars symbols args)))) body))
 
-(defn eval-application [env [f & args]]
-  (let [f-evaluated (user/eval env f)]
-    (cond
-      (fn? f-evaluated) (apply f-evaluated (eval-many env args))
-      (closure? f-evaluated) (eval-closure env f-evaluated (eval-many env args)))))
+(defn eval-macro [env [_ clo] args]
+  (evaluate env
+            (evaluate env
+                      (concat [clo] (map (partial list 'quote) args)))))
 
 (defn eval-def [env [_ k v]]
   (assert (symbol? k) (format "expected k = %s to be a symbol" k))
-  (swap! env assoc-in [:globe k] (user/eval env v)))
+  (swap! env assoc-in [:globe k] (evaluate env v)))
 
 (defn eval-if [env [_ tf t f]]
-  (let [et (user/eval env tf)]
-    (user/eval env (if et t f))))
+  (let [et (evaluate env tf)]
+    (evaluate env (if et t f))))
 
-(defn eval [env form]
+(defn eval-application [env [f & args]]
+  (let [f-evaluated (evaluate env f)]
+    (cond
+      (fn? f-evaluated) (apply f-evaluated (eval-many env args))
+      (closure? f-evaluated) (eval-closure env f-evaluated (eval-many env args))
+      (macro? f-evaluated) (eval-macro env f-evaluated args))))
+
+(defn evaluate [env form]
   (cond
     (literal? form) form
     (quote? form) (second form)
@@ -60,15 +58,79 @@
     (if? form) (eval-if env form)
     :else (eval-application env form)))
 
+(defn eval-many [env forms] (map (partial evaluate env) forms))
+
+(def env (atom {:globe {'+     + '= = 'list list 'map map 'concat concat
+                        'first first 'second second 'not not 'fav-num 41}
+                :scope {}}))
+
+(defn -main [& args]
+  (println "welcome to lic")
+  (loop []
+    (print "lic> ")
+    (flush)
+    (println (evaluate env (read)))
+    (recur)))
+
 (comment
-  (eval env (read-string "12"))
-  (eval env (read-string "(quote (+ 1 1))"))
-  (eval env (read-string "fav-num"))
-  (eval env (read-string "(+ 1 1)"))
-  (eval env (read-string "(+ 1 (+ 1 fav-num))"))
-  (do
-    (eval env (read-string "(def second-fav (+ 1 fav-num))"))
-    (eval env (read-string "second-fav")))
-  (eval env (read-string "(if (= fav-num 41) (quote yes) (quote no))"))
-  (eval env (read-string "(((clo nil (x) (list (quote clo) scope (quote (y)) (quote (+ y x)))) 2) 4)"))
+  ; we have basic primitive support
+  (evaluate env "foo")
+  (evaluate env 1.2)
+
+  ; we also have closures
+  (evaluate env '(clo nil (x) (+ 1 x)))
+
+  ; we can also define variables via `def`
+  (let [e env]
+    (evaluate e '(def foo 'bar))
+    (evaluate e 'foo))
+
+  ; we have `if` statements
+  (evaluate env '(if true 'foo 'bar))
+
+  ; we have higher order functions like `map`
+  (evaluate env '(map first '((a b))))
+
+  ; we have macro support via `mac`
+  (evaluate env '(def unless (mac (clo nil (test v))
+                                  (list 'if (list 'not test) v))))
+  ; we can define `defmacro` to ease writing new macros
+  (evaluate env '(def defmacro
+                   (mac (clo nil (n p e)
+                             (list 'def n
+                                   (list 'mac (list 'clo nil p e)))))))
+  ; with `defmacro` we can write `unless` more concisely
+  (evaluate env '(defmacro unless (test v)
+                   (list 'if (list 'not test) v)))
+
+  ; you call macros just like functions
+  (evaluate env '(unless (= fav-num 41) (throw-error)))
+
+  ; let's write a `fn` macro
+  (evaluate env '(defmacro fn (args body)
+                   (list 'list ''clo 'scope
+                         (list 'quote args)
+                         (list 'quote body))))
+
+  ; now we can do stuff like this!
+  (evaluate env '(def make-adder (clo nil (n)
+                                      (fn (y) (+ y n)))))
+  (evaluate env '(def add-two (make-adder 2)))
+  (evaluate env '(add-two 5))
+
+  ; currently, to define functions we have to use this rather verbose syntax
+  (evaluate env '(def add-fav-num (fn (x) (+ x fav-num))))
+
+  ; let's introduce `defn`
+  (evaluate env '(defmacro defn (n args body)
+                   (list 'def n (list 'fn args body))))
+
+  ; one more cool macro
+  (evaluate env '(defmacro let (pairs body)
+                   (concat (list (list 'fn (map first pairs) body))
+                           (map second pairs))))
+
+  (evaluate env '(let ((x 1) (y 2))
+                   (+ x y)))
+
   )
